7778a31f3b4186595d72a6b8b8530e0e2c7699e7
Jinwei Zhang <jz853@cornell.edu>
2020-06-07 14:54:47 -0400

    Updatea

--- bash_PDI_VI_MS.sh
+++ bash_PDI_VI_MS.sh
@@ -4,5 +4,5 @@ read -p "Enter number of iters: " niter
 
 for patientID in {1..7};
 do
-    python main_QSM_patient_all.py --gpu_id=$id --lambda_tv=20 --niter=$niter --flag_test=1 --flag_r_train=1 --patient_type='MS_new' --patient_ID=$patientID
+    python main_QSM_patient_all.py --gpu_id=$id --lambda_tv=20 --niter=$niter --flag_test=1 --flag_r_train=1 --patient_type='MS_new' --patientID=$patientID
 done
